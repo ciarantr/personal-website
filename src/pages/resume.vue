2708358f615f5a87bc5b173eea6ab7063bf7ff9b
Ciaran Toner <58338331+ciaran-io@users.noreply.github.com>
2022-11-21 20:55:05 +0000

    feat: add resume page
    1. Displays pdf of resume.

--- src/pages/resume.vue
+++ src/pages/resume.vue
@@ -1,5 +1,16 @@
-<script setup></script>
+<script setup>
+  definePageMeta({ layout: false })
+  useHead({
+    title: 'Resume.pdf',
+  })
+</script>
 
 <template>
-  <div class=""></div>
+  <div class="h-screen bg-base-30">
+    <embed
+      class="h-full w-full"
+      src="/data/ciaran-toner-resume.pdf"
+      type="application/pdf"
+    />
+  </div>
 </template>
