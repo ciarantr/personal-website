e3f9a704e0f52ebce165b773905c3b5395cf8793
Ciaran Toner <58338331+ciaran-io@users.noreply.github.com>
2022-11-21 19:40:08 +0000

    feat: add mail link & contact status

--- src/components/ContactCard.vue
+++ src/components/ContactCard.vue
@@ -1,5 +1,32 @@
-<script setup></script>
+<script setup>
+  const contactCard = $ref(null)
+</script>
 
 <template>
-  <div class=""></div>
+  <div
+    ref="contactCard"
+    class="h-96 space-y-6 overflow-clip rounded-xl p-8 shadow-2xl transition-shadow duration-1000 ease-in-out"
+  >
+    <div>
+      <h2>Whats next?</h2>
+    </div>
+    <p class="mx-auto max-w-prose">
+      I am a focused, motived web developer keen to learn & improve my developer
+      skills daily. I am currently seeking a junior web development role. If you
+      have a position available or would like to get in touch please feel free
+      to contact me.
+    </p>
+    <!-- Mailto -->
+    <nuxt-link
+      to="mailto:hi@ciaran-io.dev"
+      class="G-container-illuminate flex items-center gap-x-2 !p-2.5 [:hover&_:nth-child(2)]:grow [:hover&_:nth-child(2)]:translate-x-[96%] [:hover&_:nth-child(2)]:text-orange [:hover&_:nth-child(2)]:opacity-100"
+      >Let's connect
+      <Icon name="mdi:mail" />
+      <div
+        class="transform-gpu opacity-0 transition-all duration-500 ease-in-out"
+      >
+        <Icon name="ic:baseline-arrow-forward-ios" />
+      </div>
+    </nuxt-link>
+  </div>
 </template>
