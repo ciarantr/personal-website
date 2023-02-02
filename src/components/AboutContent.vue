8f2f77abcf719fdbac3271b043e1addff687dfc2
Ciaran Toner <58338331+ciaran-io@users.noreply.github.com>
2023-02-02 13:27:06 +0000

    refactor: add ts types, update about text
    1. Add TypeScript types.
    2. Update about description.
    3. Update profile image.
    4. Set years as experience to reactive variable.
    5. Clean up scoped styles.

--- src/components/AboutContent.vue
+++ src/components/AboutContent.vue
@@ -1,101 +1,104 @@
-<script setup>
-  const profileImageContainer = $ref(null)
+<script setup lang="ts">
+  const profileImageContainer = ref<HTMLDivElement | null>(null)
+  const yearsOfExperience = computed(() => {
+    const currentYear = new Date().getFullYear()
+    const firstYearOfExperience = 2020
+    return currentYear - firstYearOfExperience
+  })
 
   onMounted(() => {
-    const profileImage = profileImageContainer.firstChild
+    const profileImage = profileImageContainer.value?.firstElementChild
     // Add a class to the profile image & border
     //  when intersection observer is triggered
     const observer = new IntersectionObserver(
       (entry) => {
-        profileImage.classList.toggle('filter-none', entry[0].isIntersecting),
-          profileImageContainer.classList.toggle(
-            'border-shadow',
-            entry[0].isIntersecting
-          )
-        // remove the observer after the first time it runs
-        if (entry[0].isIntersecting) {
-          observer.unobserve(entry[0].target)
-        }
+        profileImage?.classList.toggle('!grayscale-0', entry[0].isIntersecting)
+        profileImageContainer.value?.classList.toggle(
+          'border-shadow',
+          entry[0].isIntersecting
+        )
       },
       { threshold: 1 }
     )
-
-    observer.observe(profileImageContainer)
+    if (profileImageContainer.value)
+      observer.observe(profileImageContainer.value)
   })
 </script>
 
 <template>
   <div
-    class="G-container-shadow G-container-shadow-spacing-base space-y-8 md:grid md:grid-cols-[50%,auto] md:space-y-0"
+    class="G-container-shadow G-container-shadow-spacing-base space-y-8 md:grid md:grid-cols-[60%,auto] md:space-y-0"
   >
     <div
+      id="profile-image-container"
       ref="profileImageContainer"
-      class="relative h-max md:ml-auto"
+      class="relative h-max md:place-self-center"
     >
       <nuxt-img
         provider="cloudinary"
         quality="100"
         loading="lazy"
-        src="/ciarant.dev/profile_2x_bvxt15.jpg"
-        width="175"
-        height="175"
-        class="relative z-20 rounded-md fill-none grayscale transition-all delay-150 duration-500 ease-in-out"
+        src="/ciarant.dev/portfolio-ciaran.jpg"
+        width="125"
+        height="125"
+        class="delay-125 relative z-20 rounded-md bg-base-40 grayscale transition-all duration-500 ease-in-out"
         alt="picture of me in my health store business"
         :modifiers="{ dpr: 2.0, f: 'auto' }"
       />
     </div>
-    <p
-      class="first-letter:float-left first-letter:mr-2 first-letter:text-6xl first-letter:text-base-20 md:row-start-1"
-    >
-      Welcome to my personal portfolio. I first began my coding journey back in
-      2018 when I needed to develop a website for my health store business. I
-      started with a small Wordpress website to reach out to customers and offer
-      products online.
-      <br /><br />
-      Fast forward to 2020 & I decided to follow my passion and made the
-      decision to change careers & become a web developer. Currently i'm
-      studying full stack web development with the
-      <nuxt-link
-        class="underline-offset-3 inline-block underline after:content-['_↗']"
-        to="https://codeinstitute.net"
-        target="blank"
-        >Code Institute</nuxt-link
-      >
-      with the goal of receiving a diploma in full stack software development.
-      <br /><br />
-      The skills I obtained over the last few years are predominately focused on
-      frontend web development; I have however built web applications using
-      backend technologies such as node.js & worked with databases, servers &
-      API's. Write now my focus is on design & development of websites, however
-      I would like to learn more about JavaScript data structures & algorithms
-      along with Python for automation & more.
-      <br /><br />
-      I have a passion for web design, user experience & web performance & look
-      forward to taking on new challenges, improving my knowledge & keeping up
-      with latest web technologies and web standards.
-    </p>
+
+    <div class="md:row-start-1">
+      <p>
+        <span class="float-left mr-2 text-6xl text-base-20">Hi,</span> my name
+        is Ciaran, a web developer from Dublin, Ireland. I currently have
+        {{ yearsOfExperience }} years experience in web development, & worked on
+        a wide range of projects from small static websites to large scale web
+        applications using a wide range of technologies and frameworks including
+        Vue.js, Node, Express, MongoDB, MySQL, TailwindCSS, Bootstrap, jQuery,
+        HTML, CSS, JavaScript, TypeScript, Git, Heroku, Netlify, Vercel, Nginx,
+        Linux, and more.
+      </p>
+      <br />
+
+      <p>
+        I have a passion for web development, & delivering high quality,
+        performant, and accessible web applications. I am currently studying at
+        the
+        <NuxtLink
+          class="underline hover:text-base-90"
+          target="_blank"
+          to="https://codeinstitute.net"
+          >Code Institute
+        </NuxtLink>
+        <icon name="lucide:external-link" />
+        to improve my knowledge and skills in full-stack web development.
+      </p>
+    </div>
   </div>
 </template>
 
 <style scoped lang="postcss">
   /* border after image */
-  div:first-of-type > div::after {
-    @apply /*   
+  #profile-image-container::after {
+    @apply /*
     width
-    of 
-    the
-    image 
-    */  absolute left-4 top-2 z-0 h-full w-[170px]
-    rounded-md 
-    border-2 
+    of
+    image*/
+    absolute
+    left-4 top-2 z-0 h-full w-[125px]
+    rounded-md
+    border-2
     border-base-40
     py-2
     transition-all duration-500 ease-in-out
-     content-[''];
+    content-[''];
   }
-  /* border shadow after image */
+
+  /* border shadow after image
+    Added with intersection observer
+  */
   .border-shadow::after {
-    @apply shadow-md 
+    @apply shadow-md
     shadow-orange;
   }
 </style>
