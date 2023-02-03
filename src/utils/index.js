77d157e96b3dca6acc5f56115081a921ea57ee61
Ciaran Toner <58338331+ciaran-io@users.noreply.github.com>
2023-02-03 11:31:54 +0000

    fix: docstring param types

--- src/utils/index.js
+++ src/utils/index.js
@@ -1,9 +1,9 @@
 /**
  * Observe node in DOM, add class when DOM property is
  * inserting viewport.
- * @param  {String} observeTarget DOM node to observe.
- * @param  {String} target Dom node to add CSS class
+ * @param  {HTMLDivElement} observeTarget DOM node to observe.
  * @param  {String} style CSS class to apply to target.
+ * @param  {HTMLDivElement} target Dom node to add CSS class
  * @param  {Number} threshold Number between 0.0 & 1.0
  */
 export function createObserver({ observeTarget, target, style, threshold }) {
@@ -11,7 +11,7 @@ export function createObserver({ observeTarget, target, style, threshold }) {
     (entry) => {
       target.classList.toggle(style, entry[0].isIntersecting)
     },
-    { threshold: threshold }
+    { threshold }
   )
   observer.observe(observeTarget)
 }
