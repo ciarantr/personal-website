ffef81d58c61e97b4f46247a3951d48a09b867a2
Ciaran Toner <58338331+ciaran-io@users.noreply.github.com>
2023-06-12 15:47:10 +0100

    fix: router rules

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -19,7 +19,7 @@ export default defineNuxtConfig({
   },
   routeRules: {
     '/index': {static: true},
-    'resume': {static: true},
+    '/resume': {static: true},
   '/api/github': {cors: true}
   },
   // configuration incorrect see
