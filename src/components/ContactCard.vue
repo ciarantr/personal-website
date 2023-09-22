54187d3b2e8a657b2a29b7fd044a951b5d4d4869
Ciaran Toner <58338331+ciaran-io@users.noreply.github.com>
2023-09-22 15:26:48 +0100

    feat: update for improved readability and clarity
    Changes to ContactCard.vue include modifying header size, updating
    description text, and adjusting element styling. The heading text size
    was increased to enhance visibility and make it more engaging. The
    description was updated to provide a more accurate and compelling
    representation of the professional experience and aspirations.
    Furthermore, the changes to the nuxt-link styling aimed to improve
    usability and visual appeal.

--- src/components/ContactCard.vue
+++ src/components/ContactCard.vue
@@ -16,18 +16,19 @@
     class="h-96 space-y-6 overflow-clip rounded-xl p-8 shadow-2xl transition-shadow duration-1000 ease-in-out"
   >
     <div>
-      <h2>Whats next?</h2>
+      <h2 class="text-5xl">Whats next?</h2>
     </div>
     <p class="mx-auto max-w-prose">
-      I am a focused, motived web developer keen to learn & improve my developer
-      skills daily. I am currently seeking a junior web development role. If you
-      have a position available or would like to get in touch please feel free
-      to contact me.
+      I am a dedicated and driven developer with a strong desire to continuously
+      enhance my skills in web & software development. Continuously striving to
+      expand my knowledge and sharpen my skills in this field. If you come
+      across opportunities where my skill set could be a good fit, or just want
+      to establish contact, feel free to reach out.
     </p>
     <!-- Mailto -->
     <nuxt-link
       to="mailto:hi@ciaran-io.dev"
-      class="G-container-illuminate flex items-center gap-x-2 !p-2.5 [:hover&_:nth-child(2)]:grow [:hover&_:nth-child(2)]:translate-x-[96%] [:hover&_:nth-child(2)]:text-orange [:hover&_:nth-child(2)]:opacity-100"
+      class="G-container-illuminate [:hover&_:nth-child(2)]:text-orange flex items-center gap-x-2 !p-2.5 [:hover&_:nth-child(2)]:grow [:hover&_:nth-child(2)]:translate-x-[96%] [:hover&_:nth-child(2)]:opacity-100"
       >Let's connect
       <Icon name="mdi:mail" />
       <div
