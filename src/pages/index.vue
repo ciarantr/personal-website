34f6adc6852eb0b8c6969fe2210029cc50acad46
Ciaran Toner <58338331+ciaran-io@users.noreply.github.com>
2023-09-25 16:56:55 +0100

    feat: Update homepage layout and improve accessibility
    This commit modifies the structure and styling of our homepage for a
    better user experience. Changes include alterations to the content
    layout, some text modifications, and enhancement of accessibility
    features.
    
    The script setup syntax has been updated; title now reflects the
    professional role, and template parameters have been extended to improve
     SEO. Several "aria-label" have also been updated to improve website
    accessibility for screen reader users. Some irrelevant "aria-label"s
    were removed.

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,65 +1,68 @@
-<script setup lang="ts">
+<script lang="ts" setup>
   useHead({
-    title: 'Home',
+    templateParams: {
+      separator: '•',
+    },
+    title: 'Full Stack Web Developer',
+    titleTemplate: '%siteName %separator %s',
+  })
+  //  Define og image vue template
+  defineOgImage({
+    component: 'TheBaseOg',
   })
 </script>
 
 <template>
   <main
     id="main-content"
-    aria-label="main content"
     class="mt-48"
   >
     <div class="container lg:max-w-5xl">
-      <section>
-        <div class="mt-12 space-y-32">
-          <!-- About me  -->
-          <content-container
-            id="about"
-            aria-label="about me content"
-            heading="About"
-          >
-            <template #content-main>
-              <about-content />
-            </template>
-          </content-container>
+      <div class="mt-12 space-y-32">
+        <!-- About me  -->
+        <content-container
+          id="about"
+          heading="About"
+        >
+          <template #content-main>
+            <about-content />
+          </template>
+        </content-container>
 
-          <!-- Work history -->
-          <content-container
-            id="experience"
-            aria-label="work history content"
-            heading="Were I have worked"
-          >
-            <template #content-main>
-              <job-history-container />
-            </template>
-          </content-container>
-        </div>
+        <!-- Work history -->
+        <content-container
+          id="experience"
+          heading="Were I have worked"
+        >
+          <template #content-main>
+            <job-history-container />
+          </template>
+        </content-container>
+      </div>
 
-        <!-- Projects -->
-        <section class="mt-64 space-y-12">
-          <div class="text-center">
-            <h3 class="inline text-6xl">Some things I built</h3>
-            <icon
-              class="text-base-20 ml-4 !align-bottom text-5xl"
-              name="mdi:hammer-wrench"
-            />
-          </div>
-          <project-container
-            id="projects"
-            aria-label="A curated group of apps I have made with description and links to code repositories"
+      <!-- Projects -->
+      <section
+        aria-label="completed web development projects with description & links"
+        class="mt-64 space-y-12"
+      >
+        <div class="text-center">
+          <h3 class="inline text-6xl">Some things I built</h3>
+          <icon
+            class="text-base-20 ml-4 !align-bottom text-5xl"
+            name="mdi:hammer-wrench"
           />
-        </section>
+        </div>
+        <project-container id="projects" />
       </section>
     </div>
     <!-- Technology used slide -->
     <section
       id="tech-stack"
+      aria-label="list of technologist I have used"
       class="relative mt-64 space-y-12 overflow-clip"
-      aria-label="A list of technologies I have worked with and small description"
     >
       <div class="relative z-50 text-center">
-        <h3 class="inline text-6xl">Some technologies I use</h3>
+        <h3 class="inline text-6xl">Technologies I use</h3>
         <icon
           class="text-base-20 ml-4 !align-baseline text-5xl"
           name="material-symbols:settings-suggest"
@@ -74,6 +77,7 @@
       <!-- Roadmap -->
       <section
         ref="roadmapSection"
+        aria-label='roadmap of my learning journey from 2022 to 2023'
         class="container space-y-12 lg:max-w-5xl"
       >
         <lazy-roadmap-container />
@@ -81,6 +85,7 @@
       <!-- Contact us -->
       <section
         id="contact"
+        aria-label='contact me via email'
         class="mx-auto h-full px-4 pt-32 md:w-max md:px-0"
       >
         <lazy-contact-card />
