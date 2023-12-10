cef84e0f5f4397ed02a4e958415b9dad118d8e6b
Ciaran Toner <58338331+ciaran-io@users.noreply.github.com>
2023-12-10 18:16:27 +0000

    refactor: update ContactCard component styling and content
    The ContactCard component has been updated to improve readability and
    conciseness. Styling changes were made to better utilise space and
    ensure a consistent appearance. Content was also revised to be more
    direct and inviting, making it easier for potential partners to connect.

--- src/components/ContactCard.vue
+++ src/components/ContactCard.vue
@@ -13,29 +13,28 @@
 <template>
   <div
     ref="contactCard"
-    class="sm:h-96 space-y-6 overflow-clip rounded-xl px-4 py-6 sm:p-8 shadow-2xl transition-shadow duration-1000 ease-in-out"
+    class="grid gap-y-6 overflow-clip rounded-xl px-4 py-6 shadow-2xl transition-shadow duration-1000 ease-in-out sm:h-96 sm:p-8"
   >
     <div>
       <h2 class="text-4xl sm:text-5xl">Whats next?</h2>
     </div>
     <p class="mx-auto max-w-prose">
-      I am a dedicated and driven developer with a strong desire to continuously
-      enhance my skills in web & software development. Continuously striving to
-      expand my knowledge and sharpen my skills in this field. If you come
-      across opportunities where my skill set could be a good fit, or just want
-      to establish contact, feel free to reach out.
+      Ready to explore new opportunities & challenges. Reach out anytime for
+      potential partnerships or career opportunities.
     </p>
     <!-- Mailto -->
-    <nuxt-link
-      to="mailto:hi@ciaran-io.dev"
-      class="G-container-illuminate [:hover&_:nth-child(2)]:text-orange flex items-center gap-x-2 !p-2.5 [:hover&_:nth-child(2)]:grow [:hover&_:nth-child(2)]:translate-x-[96%] [:hover&_:nth-child(2)]:opacity-100"
-      >Let's connect
-      <Icon name="mdi:mail" />
-      <div
-        class="transform-gpu opacity-0 transition-all duration-500 ease-in-out"
-      >
-        <Icon name="ic:baseline-arrow-forward-ios" />
-      </div>
-    </nuxt-link>
+    <div>
+      <nuxt-link
+        to="mailto:hi@ciaran-io.dev"
+        class="G-container-illuminate [:hover&_:nth-child(2)]:text-orange flex items-center gap-x-2 !p-2.5 [:hover&_:nth-child(2)]:grow [:hover&_:nth-child(2)]:translate-x-[96%] [:hover&_:nth-child(2)]:opacity-100"
+        >Let's connect
+        <Icon name="mdi:mail" />
+        <div
+          class="transform-gpu opacity-0 transition-all duration-500 ease-in-out"
+        >
+          <Icon name="ic:baseline-arrow-forward-ios" />
+        </div>
+      </nuxt-link>
+    </div>
   </div>
 </template>
