6e95ce1233c9202d000cbe3f4670d85a471ebb5c
Ciaran Toner <58338331+ciaran-io@users.noreply.github.com>
2023-12-10 18:25:18 +0000

    Refactor: header component and update styling
    The header component of TheHeader.vue has been simplified and refactored
    . The navigation menu mechanism is changed to use 'useMenu()' instead of
     a boolean ref. Removed unnecessary classes and added the use of svg
     over spans in the menu. The overall structure of the mobile and desktop
      navigation is improved for a better user experience. The styling is
      also updated for a cleaner and more modern look.

--- src/components/TheHeader.vue
+++ src/components/TheHeader.vue
@@ -1,13 +1,7 @@
-<script setup>
+<script setup lang="ts">
   // TODO:(Enhancement) Change menu spans to svg
-  const isMobileMenuOpen = ref(false)
-  const navigation = new Set([
-    'about',
-    'experience',
-    'projects',
-    'contact',
-    'resume',
-  ])
+  const isMobileMenuOpen = useMenu()
+  const navigation = new Set(['about', 'experience', 'projects'])
 
   const mobileSize = 640
 
@@ -23,7 +17,7 @@
 </script>
 
 <template>
-  <header class="container sm:relative">
+  <header class="container">
     <nuxt-link
       to="/#main-content"
       class="bg-base-20 fixed left-0 top-0 z-[100] w-full -translate-y-full p-1 text-center transition-transform focus:translate-y-0"
@@ -33,19 +27,19 @@
     <nav
       id="primary"
       aria-label="main"
-      class="G-container-shadow duration-350 mt-6 items-center transition-[height] ease-in-out sm:relative sm:flex sm:justify-between sm:gap-y-0"
+      class="G-container-shadow duration-350 mt-6 transition-[height] ease-in-out sm:grid sm:grid-flow-col sm:items-baseline md:items-center md:transition-none"
       :class="{
-        'h-[64px] ': !isMobileMenuOpen,
-        'absolute left-0 right-0 top-0 z-50 mx-2 h-[95vh] space-y-8':
+        'relative h-[64px]': !isMobileMenuOpen,
+        'absolute left-0 right-0 top-0 z-50 mx-2 h-[500px] space-y-8':
           isMobileMenuOpen,
       }"
     >
-      <div class="flex items-center gap-x-2 sm:w-auto">
-        <Icon
-          name="arcticons:cinexplore"
-          class="h-8 w-8"
-        />
-
+      <div class="flex items-baseline md:block">
+        <span
+          id="name"
+          class="align-middle text-sm"
+          >Ciaran Toner</span
+        >
         <!-- Mobile menu button -->
         <button
           aria-controls="primary"
@@ -70,8 +64,7 @@
             <span
               :class="{
                 'w-full': !isMobileMenuOpen,
-                'w-[calc(28px_*_1.2)] -rotate-45 [translate:_-3px_-3px]':
-                  isMobileMenuOpen,
+                'w-[25px] -rotate-45 [translate:_-3px_-3px]': isMobileMenuOpen,
               }"
               class="top-4"
             ></span>
@@ -79,20 +72,19 @@
             <span
               :class="{
                 'w-4/6': !isMobileMenuOpen,
-                'w-[calc(28px_*_1.2)] rotate-45 [translate:_-3px_-11px] ':
-                  isMobileMenuOpen,
+                'w-[25px] rotate-45 [translate:_-3px_-11px] ': isMobileMenuOpen,
               }"
               class="top-6"
             ></span>
           </span>
         </button>
       </div>
-      <!-- Main nav menu -->
+      <!-- Main nav menu mobile/desktop -->
       <div
-        class="text-lg capitalize transition-opacity delay-150 ease-in sm:visible sm:flex sm:gap-x-4 sm:text-base sm:opacity-100 sm:transition-none"
+        class="justify-center text-lg capitalize transition-opacity delay-150 ease-in sm:flex sm:items-center sm:gap-x-4 sm:text-base sm:transition-none"
         :class="{
-          'visible  block h-auto opacity-100': isMobileMenuOpen,
-          'invisible hidden  h-0 opacity-0 ': !isMobileMenuOpen,
+          block: isMobileMenuOpen,
+          hidden: !isMobileMenuOpen,
         }"
       >
         <ul
@@ -100,69 +92,138 @@
             'G-container-shadow G-container-shadow-spacing-base':
               isMobileMenuOpen,
           }"
-          class="divide-base-20 flex flex-col divide-y text-sm sm:flex-row sm:items-center sm:gap-x-6 sm:gap-y-0 sm:divide-y-0"
+          class="divide-base-20 flex flex-col gap-y-2 divide-y text-sm sm:flex-row sm:items-center sm:gap-x-6 sm:gap-y-0 sm:divide-y-0"
         >
           <li
             v-for="list in navigation"
             :key="list"
-            class="py-1.5 sm:py-0"
-            :class="{ hidden: list === 'resume' && isMobileMenuOpen }"
+            class="flex items-center justify-center pt-2 md:block md:pt-0"
           >
             <a
               :aria-label="`${list} section`"
-              :href="list === 'resume' ? '/resume' : `#${list}`"
-              :target="list === 'resume' ? '_blank' : '_self'"
+              :href="`#${list}`"
               class="G-border-animate block w-full"
               @click="isMobileMenuOpen ? (isMobileMenuOpen = false) : null"
               >{{ list }}</a
             >
           </li>
         </ul>
-        <!-- Resume download / view mobile -->
+        <!-- Resume / linkedin / github -->
         <div
           class="G-container-shadow G-container-shadow-spacing-base mt-3 space-y-2 sm:hidden"
         >
           <!-- view pdf -->
-          <ul class="text-sm">
+          <ul class="space-y-4 text-sm">
             <li>
               <nuxt-link
                 to="/resume"
-                target='_blank'
-                class="flex items-center justify-between pt-1"
-              >Resume
+                target="_blank"
+                class="flex items-center justify-between"
+                >Resume
+                <Icon name="ion:chevron-forward-outline" />
+              </nuxt-link>
+            </li>
+            <li>
+              <nuxt-link
+                to="https://github.com/ciaran-io"
+                target="_blank"
+                class="flex items-center justify-between"
+                >GitHub
+                <ExternalLinkIcon />
+              </nuxt-link>
+            </li>
+            <li>
+              <nuxt-link
+                to="https://www.linkedin.com/in/ciaran-toner/"
+                target="_blank"
+                class="flex items-center justify-between"
+                >Linkedin
+                <ExternalLinkIcon />
               </nuxt-link>
             </li>
           </ul>
         </div>
       </div>
+
+      <!-- linkedin / github links -->
+      <div class="hidden items-center justify-end gap-x-3 sm:flex">
+        <nuxt-link
+          to="https://github.com/ciaran-io"
+          target="_blank"
+          class="hover:text-base-20 transition-colors duration-200"
+        >
+          <Icon
+            name="icon-park-outline:github"
+            class="h-5 w-5"
+          />
+        </nuxt-link>
+
+        <nuxt-link
+          to="https://www.linkedin.com/in/ciaran-toner/"
+          target="_blank"
+          class="hover:text-base-20 transition-colors duration-200"
+        >
+          <Icon
+            name="brandico:linkedin-rect"
+            class="h-5 w-5"
+          />
+        </nuxt-link>
+      </div>
     </nav>
 
     <section
       aria-label="banner"
-      class="G-container-shadow mt-16 py-28 text-center"
+      class="G-container-shadow mt-16 py-14 md:py-28"
+      :class="{
+        'blur-sm': isMobileMenuOpen,
+      }"
     >
-      <div class="space-y-12">
-        <h1 class="!text-base-70 text-center text-3xl md:text-4xl">
-          Ciaran Toner
-          <span class="text-base-20 block text-xl md:inline"
-            >Full stack web developer</span
+      <div class="space-y-12 px-3 md:mx-auto md:w-max md:pl-6">
+        <div
+          id="main-heading-text"
+          class="relative text-left"
+        >
+          <h1
+            class="text-base-90/70 relative max-w-[20ch] pb-1 text-3xl sm:inline-block md:text-5xl"
           >
-        </h1>
-        <div class="flex justify-center">
-          <div
-            id="wrapper-text"
-            class="relative"
-          >
-            <p
-              class="relative bg-gradient-to-r from-gray-400/50 via-gray-400/60 to-gray-400 bg-clip-text pb-1 text-center text-xl text-transparent sm:inline-block sm:max-w-[40ch]"
-            >
-              Building modern full stack web applications for today's web
-            </p>
+            Building Full-Stack Applications For Today's Web.
+          </h1>
+          <Icon
+            class="[&>circle]:text-orange text-base-20 h-12 w-12 !align-sub md:mb-2 [&>circle]:animate-[pulse_3s_ease-in-out_infinite]"
+            name="clarity:world-outline-badged"
+          />
+        </div>
+
+        <p class="text-base-10/70 text-lg md:w-[50ch]">
+          Pioneering digital frontiers through adept full-stack web development.
+          I invite you to explore my diverse selection of projects, get a taste
+          of the cutting-edge technologies I employ, and gain insights into my
+          ambitious future roadmap — a testament to my relentless quest for
+          growth and innovation.
+        </p>
+        <!-- Resume contact email -->
+        <div class="flex items-baseline gap-x-4">
+          <nuxt-link
+            to="/resume"
+            target="_blank"
+            class="bg-base-20 hover:shadow-base-70/50 group flex items-center gap-x-1 rounded px-2.5 py-2 shadow-lg shadow-transparent transition-all duration-300 ease-in-out"
+            >Resume
             <Icon
-              class="[&>circle]:text-orange text-base-20 h-5 w-5 !align-sub md:mb-2 [&>circle]:animate-[pulse_3s_ease-in-out_infinite]"
-              name="clarity:world-outline-badged"
+              class="group-hover:text-orange inline-block h-5 w-5 transition-colors duration-300 md:text-transparent"
+              name="mynaui:external-link"
             />
-          </div>
+          </nuxt-link>
+          <!-- email -->
+          <nuxt-link
+            to="mailto:hi@ciaran-io.dev"
+            class="bg-base-20 hover:shadow-base-70/50 group flex items-center gap-x-1 rounded px-2.5 py-2 shadow-lg shadow-transparent transition-all duration-300 ease-in-out"
+          >
+            Contact
+            <Icon
+              class="group-hover:text-orange inline-block h-5 w-5 transition-colors duration-300 md:text-transparent"
+              name="mingcute:send-plane-fill"
+            />
+          </nuxt-link>
         </div>
       </div>
     </section>
@@ -170,31 +231,11 @@
 </template>
 
 <style lang="postcss" scoped>
-  h1 {
-    @apply text-base-20;
-
-    /* Bracket gradient */
-    span::before,
-    span::after {
-      @apply via-base-40
-      to-base-45
-      bg-gradient-to-b
-      from-transparent
-      bg-clip-text
-      text-transparent;
-    }
-
-    span::before {
-      content: '( ';
-    }
-
-    span::after {
-      content: ' )';
-    }
+  #name {
+    font-family: 'Julius Sans One', sans-serif;
   }
-
   /* Underline border gradient */
-  #wrapper-text::after {
+  #main-heading-text::after {
     @apply via-base-40
     to-base-80
     absolute
