e28f5f6eab72187b7e513c4dbb5b4d60d512f9f8
Ciaran Toner <58338331+ciaran-io@users.noreply.github.com>
2024-05-29 09:05:40 +0100

    fix: tab animations on mobile in production

--- src/components/JobHistoryTab.vue
+++ src/components/JobHistoryTab.vue
@@ -1,4 +1,5 @@
 <script setup lang="ts">
+  // TODO: refactor updateActiveTab
   const emit = defineEmits(['changeTab'])
 
   defineProps({
@@ -16,20 +17,35 @@
   function emitData(index: number) {
     emit('changeTab', index)
   }
-  const tabWidth = ref(0);
-  const offset = ref(0);
+  const tabWidth = ref(null as number | null);
+  const offset = ref(20);
+
+ // Function to update dimensions and active tab
+function updateActiveTab( event: MouseEvent) {
+  activeAndInitialValues(event.target as HTMLElement);
+}
+
+// Function to set both initial and active values
+function activeAndInitialValues(element: HTMLElement) {
+  tabWidth.value = element.clientWidth;
+  offset.value = element.offsetLeft;
+}
 </script>
 
 <template>
   <ul
     role="tablist"
-    class="tab-container G-container-shadow G-container-shadow-spacing-base relative row-start-2 grid gap-x-8 md:gap-x-0  grid-flow-col items-baseline overflow-x-auto md:row-start-1 md:mt-0 md:grid-flow-row"
+    class="tab-container G-container-shadow G-container-shadow-spacing-base relative row-start-2 grid gap-x-8 md:gap-x-0  grid-flow-col items-baseline  md:row-start-1 md:mt-0 md:grid-flow-row  overflow-x-auto md:overflow-x-hidden"
   >
     <li
-      v-for="(title, index) in tabTitle"
-      :id="`tab-${index + 1}`"
-      :key="index"
-      class="relative h-full min-w-max cursor-pointer text-center transition-colors duration-700 md:grid md:items-center md:text-left "
+    v-for="(title, index) in tabTitle"
+    :id="`tab-${index + 1}`"
+    :key="index"
+      :style="{
+        '--offset': `${offset - 20}px`,
+         '--tab-width': tabWidth ? `${tabWidth}px` : '100%' 
+        }"
+      class="relative md:h-full min-w-max cursor-pointer text-center transition-colors duration-700 md:grid md:items-center md:text-left "
       role="tab"
       tabindex="0"
       :aria-controls="`panel-${index + 1}`"
@@ -39,9 +55,7 @@
         { 'active-tab': activeTab === index },
       ]"
       @keyup.enter="emitData(index)"
-      @click="emitData(index), 
-      tabWidth = $event.target.clientWidth, 
-      offset = $event.target.offsetLeft"
+      @click="emitData(index),  updateActiveTab( $event)"
     >
       {{ title }}
     </li>
@@ -56,17 +70,12 @@
     block
     h-[1.5px]
     rounded-md
-    /* transition-[left] */
     duration-500
     content-[''];
     top: -14px;
     transition: left 500ms, width 500ms;
-     /* FIXME: Fix the left position of the tab indicator with the ternary operator 
-     offset is offsetLeft of the tab element 
-     20 is the padding of the tab container 
-     */
-    left: v-bind(offset ? (offset - 20) + 'px' : '0px') ;
-    width: v-bind(tabWidth ? tabWidth   + 'px' : '100%');
+    left: var(--offset);
+    width: var(--tab-width);
 
     /* On screen sizes => 768px */
     @screen md {
